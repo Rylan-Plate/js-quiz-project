2cf50d22c00ea9e05ed77b94fa118885f3add9f8
Rylan-Plate <99075330@mydavinci.nl>
2026-01-29 09:31:31 +0100

    added buttons and disabled question/answers on default
    added buttons
    disabled question/answers on default
    forgot to connect js to html

---
+++ quiz.js
@@ -0,0 +1,5 @@
+document.getElementById("q").style.display = "none";
+document.getElementById("a1").style.display = "none";
+document.getElementById("a2").style.display = "none";
+document.getElementById("a3").style.display = "none";
+document.getElementById("a4").style.display = "none";
