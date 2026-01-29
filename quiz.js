2dc274e034133bcc13ee5e272ae0f29f92e8769f
Rylan-Plate <99075330@mydavinci.nl>
2026-01-29 11:54:28 +0100

    Verwerking van de antwoordselectie en scoring

--- quiz.js
+++ quiz.js
@@ -16,8 +16,8 @@ let correctAnswers = [1, 0, 0, 0, 2]
 
 let currentQuestion = 0
 let score = 0
-
 function showQuestionsAndAnswers(questionArray, currentQuestion, answerArray){
+    
     document.getElementById("question").innerHTML = questionArray[currentQuestion]
     document.getElementById("a1").innerHTML = answerArray[currentQuestion][0]
     document.getElementById("a2").innerHTML = answerArray[currentQuestion][1]
@@ -25,6 +25,25 @@ function showQuestionsAndAnswers(questionArray, currentQuestion, answerArray){
     document.getElementById("a4").innerHTML = answerArray[currentQuestion][3]
     return currentQuestion + 1
 }
+function correctCheck(correctAnswers, currentQuestion, answerArray, score){
+    let givenAnswer = document.getElementById("answers").value
+    if (givenAnswer == correctAnswers[currentQuestion-1]){
+        document.getElementById("feedback").innerHTML = "Goed gedaan! Antwoord " + answerArray[currentQuestion-1][givenAnswer] + " is juist!"
+        score += 1
+        console.log(score)
+        document.getElementById("currentScore").innerHTML = "Huidige score = " + score
+    }
+    else {
+       document.getElementById("feedback").innerHTML = "Het door jou ingevoerde antwoord is... Onjuist!"
+    }
+    return [currentQuestion = showQuestionsAndAnswers(questionArray, currentQuestion, answerArray), score]
+}
+console.log(score)
+
 document.getElementById("nextQuestion").addEventListener("click", () =>{
-    currentQuestion = showQuestionsAndAnswers(questionArray, currentQuestion, answerArray)
+    returnedValues = correctCheck(correctAnswers, currentQuestion, answerArray, score)
+    currentQuestion = returnedValues[0]
+    score = returnedValues[1]
 })
+
+currentQuestion = showQuestionsAndAnswers(questionArray, currentQuestion, answerArray)
