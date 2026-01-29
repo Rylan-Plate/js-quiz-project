66617b34b055f813ea347c09d28de333ef26aeec
Rylan-Plate <99075330@mydavinci.nl>
2026-01-29 09:40:22 +0100

    added start button
    added start button to enable the question text (which is empty on default) and to enable the answer buttons

--- quiz.js
+++ quiz.js
@@ -1,5 +1,14 @@
-document.getElementById("q").style.display = "none";
-document.getElementById("a1").style.display = "none";
-document.getElementById("a2").style.display = "none";
-document.getElementById("a3").style.display = "none";
-document.getElementById("a4").style.display = "none";
+document.getElementById("q").style.display = "none"
+document.getElementById("a1").style.display = "none"
+document.getElementById("a2").style.display = "none"
+document.getElementById("a3").style.display = "none"
+document.getElementById("a4").style.display = "none"
+
+document.getElementById("startBtn").addEventListener("click", () => {
+    document.getElementById("q").style.display = ""
+    document.getElementById("a1").style.display = ""
+    document.getElementById("a2").style.display = ""
+    document.getElementById("a3").style.display = ""
+    document.getElementById("a4").style.display = ""
+    document.getElementById("startBtn").style.display = "none"
+})
